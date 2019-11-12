0945293fae837e16994ff6f68d38f17c41439d78
aayush.bhargava <aayush.bhargava@jungleworks.com>
2019-11-12 21:41:51 +0530

    Base App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ModalService } from './modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss','./jw-modal/jw-modal.component.scss']
 })
 export class AppComponent {
   title = 'crud-master';
+  constructor(private modalService: ModalService){
+    
+  }
+  openModal(id: string) {
+    this.modalService.open('123');
+}
+closeModal(id: string) {
+  this.modalService.close('123');
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JwModalComponent } from './jw-modal/jw-modal.component';
+import { ModalService } from './modal.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JwModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  exports: [JwModalComponent],
+  providers: [ModalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
